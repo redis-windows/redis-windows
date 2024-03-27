d8ee65762c28d293e4c8be5c6d708b9571fd0eb8
yes8080 <wsmsn@msn.com>
2024-03-27 15:52:47 +0800

    Create uninstall_redis_service.bat

---
+++ uninstall_redis_service.bat
@@ -0,0 +1,30 @@
+@echo off
+SET REDIS_PATH=%~dp0
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%REDIS_PATH%"
+:--------------------------------------
+
+sc stop "Redis"
+sc delete "Redis"
+
+pause
